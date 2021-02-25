d68b882c181681fb3e8465a589645be9cb1992cc
joeroybal <joewroybal@gmail.com>
2021-02-24 19:20:36 -0600

    solved views issue

--- app/views.py
+++ app/views.py
@@ -7,6 +7,6 @@ def index():
     return render_template("index.html")
 
 
-# @app.route('/about')
-# def about():
-#     return render_template("about.html")
+@app.route('/about')
+def about():
+    return render_template("about.html")
