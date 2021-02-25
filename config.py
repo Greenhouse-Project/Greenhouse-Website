9b8efa334df96215092767f10e15db3e3ca62118
joeroybal <joewroybal@gmail.com>
2021-02-24 18:58:30 -0600

    Set up framework using Flask Current issue with the "views.py" page

---
+++ config.py
@@ -0,0 +1,5 @@
+'''
+This file contains all of the config variables for the app
+'''
+# Enable Flask's debugging features. (Default is FALSE)
+DEBUG = True
---
+++ run.py
@@ -0,0 +1,8 @@
+'''
+This is the application's entry point. 
+This file will be used to start the Flask server and launch the application.
+'''
+from app import app
+
+if __name__ == '__main__':
+    app.run()
