2d66042001c2f98b80b9958014a871c14f302108
joeroybal <joewroybal@gmail.com>
2021-02-24 21:59:29 -0600

    test
    A#	modified:   env/lib/python3.8/site-packages/pip/_internal/index/__init__.py

--- config.py
+++ config.py
@@ -1,5 +1,27 @@
 '''
 This file contains all of the config variables for the app
 '''
-# Enable Flask's debugging features. (Default is FALSE)
-DEBUG = True
+class Config(object):
+    """
+    Common configurations:
+    Put any configs here that are common accross all environments
+    """
+    
+class DevConfig(Config):
+    """
+    Development configurations
+    """
+    DEBUG = True
+    SQLALCHEMY_ECHO = True # Logging SQL errors
+    SQLALCHEMY_DATABASE_URI = True # Make sure you are connecting to the right address
+    
+class ProductionConfig(Config):
+    """
+    Production configurations
+    """
+    DEBUG = False
+    
+app_config ={
+    'development': DevConfig,
+    'production': ProductionConfig
+}
