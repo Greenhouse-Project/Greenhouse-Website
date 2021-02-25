b4e48af24919e917e62d7a7fdd904beb23affe2c
joeroybal <joewroybal@gmail.com>
2021-02-24 19:08:11 -0600

    working in views.py

--- app/views.py
+++ app/views.py
@@ -1,12 +1,12 @@
-# from flask import render_template
-# from app import app
+from flask import render_template
+from app import app
 
 
-# @app.route('/')
-# def index():
-#     return render_template("index.html")
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 
-# # @app.route('/about')
-# # def about():
-# #     return render_template("about.html")
+# @app.route('/about')
+# def about():
+#     return render_template("about.html")
