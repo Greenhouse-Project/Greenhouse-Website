b751aca378bcd06cc5dfcac03b91c437ece6e45c
joeroybal <joewroybal@gmail.com>
2021-02-26 13:03:18 -0600

    got CSS to show up on page needs "  type="text/css" href="{{ url_for('static', filename='main.css') }}"    " in the HTML

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask import render_template # needed for displaying the HTML file
 
 # Local import
 from config import app_config
-# from app import views
+# from app import views   # might not be needed
 
 # Database variable
 db =  SQLAlchemy()
@@ -14,9 +15,31 @@ def create_app(config_name):
     app.config.from_pyfile('config.py')
     db.init_app(app)
     
-    #testing route
+    # Home route
     @app.route('/')
-    def test():
-        return 'It works!'
+    def Home():
+        return render_template('index.html', title='Home')
+    
+    # # Front Room route
+    # @app.route('/front-room')
+    # def Front Room():
+    #     return render_template('TBD', title='Front Room')
+    
+    # # Back Room route
+    # @app.route('/back-room')
+    # def Back Room():
+    #     return render_template('TBD', title='Back Room')
+    
+    # # Outside route
+    # @app.route('/outside')
+    # def Outside():
+    #     return render_template('TBD', title='Outside')
+    
+    # About route
+    @app.route('/about')
+    def About():
+        return 'The About page is working'
+    #     return render_template('TBD', title='About')
+    
     
     return app
