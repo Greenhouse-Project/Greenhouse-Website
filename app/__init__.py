5d758474d793680e8d0d52f9ca0272bebee28960
joeroybal <joewroybal@gmail.com>
2021-02-26 11:46:50 -0600

    integrated flask and possibly MySQL

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-# Initialize the app
-app = Flask(__name__, instance_relative_config=True)
+# Local import
+from config import app_config
 
-# Load th config file
-app.config.from_object('config')
+# Database variable
+db =  SQLAlchemy()
+
+def create_app(config_name):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
+    
+    return app
--- run.py
+++ run.py
@@ -2,7 +2,11 @@
 This is the application's entry point. 
 This file will be used to start the Flask server and launch the application.
 '''
-from app import app
+import os
+from app import create_app
+
+config_name = os.getenv('FLASK_CONFIG')
+app = create_app(config_name)
 
 if __name__ == '__main__':
     app.run()
